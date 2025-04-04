84f0e0975606f60726feff4ae628a8c95ac3ab55
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-04-04 23:01:19 +0530

    made changes

---
+++ src/components/Xdisplay.js
@@ -0,0 +1,62 @@
+import {useState} from "react";
+
+export function Xdisplay(){
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: ""
+    });
+    const [fullName, setFullName] = useState("");
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+        console.log(formData);
+    };
+
+    const handleSubmite = (e) => {
+        e.preventDefault();
+        setFullName(`${formData.firstName} ${formData.lastName}`);
+        console.log(fullName);
+    };
+
+    return(
+        <div>
+            <h1>Full Name Display</h1>
+            <form onSubmit = {handleSubmite}>
+                <div>
+                    <lable>
+                        Firt Name:{" "}
+                        <input 
+                            type = "text"
+                            name = "firstName"
+                            value = {formData.firstName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </lable>
+                </div>
+                <div>
+                    <lable>
+                        Last Name:{" "}
+                        <input 
+                            type = "text"
+                            name = "lastName"
+                            value = {formData.lastName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </lable>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            {fullName && (
+                <p> Full Name: {fullName}</p>
+            )}
+        </div>
+    );
+
+    
+}
